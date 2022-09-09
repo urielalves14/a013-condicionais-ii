92fdf868cf9fbb808cb7e3d66dbc8e9e58274417
urielalves14 <alvesuriel57@gmail.com>
2022-09-08 21:57:15 -0300

    atv13

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,33 @@
-let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
+let num 
 
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
+const nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
+
+switch(nacionalidade){
+    case "brasileira":
+         num = "A pessoa é do Brasil"
+    break
+    case "chilena":
+        num = "A pessoa é do Chile"
+    break
+    case "argentina":
+       num = "A pessoa é da Argentina"
+    break
+    case "uruguaia":
+       num = "A pessoa é do Uruguai"
+    break
+    case "colombiana":
+       num = "A pessoa é da Colombia"
+
 }
+console.log(num)
+// } else if(nacionalidade === "argentina"){
+//     console.log("a pessoa é da Argentina!");
+// } else if(nacionalidade === "uruguaia"){
+//     console.log("a pessoa é do Uruguai!");
+// } else if(nacionalidade === "chilena"){
+//     console.log("a pessoa é do Chile!");
+// } else if(nacionalidade === "colombiana"){
+//     console.log("a pessoa é da Colômbia!");
+// } else{
+//     console.log("nacionalidade não encontrada")
+// }
